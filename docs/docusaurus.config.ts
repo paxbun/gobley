c16d940d9dbb81c98f541852eb976f13f337b72c
Chanjung Kim <chanjungkim@paxbun.dev>
2025-03-10 22:45:32 +0900

    Copy README.md to docs/tutorial.md

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -82,7 +82,7 @@ const config: Config = {
           items: [
             {
               label: "Tutorial",
-              to: "/docs/intro",
+              to: "/docs/tutorial",
             },
           ],
         },
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
+            to="/docs/tutorial"
           >
             Get Started in 5 minutes ⏱️
           </Link>
