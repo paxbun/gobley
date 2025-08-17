paxbun/gobley

commit b00c986a95cce54a1f84361b8a81d9a085e2cd04

Parse library search paths from build script outputs in CargoBuildTask (#113)
## Changes

- Ported `CargoMessage` from the `cargo_metadata` package.
- Made `CargoBuildTask` parse library search paths from build script
outputs using `CargoMessage`.

## Testing

Tested manually with a MinGW Kotlin/Native project. Confirmed library
search paths prefixed by `-L` are populated in `nativeStaticLibsDef`.

## Issues Fixed

Fixes: #112

build-logic/gobley-gradle-cargo/src/main/kotlin/tasks/CargoBuildTask.kt
@@ -7,6 +7,7 @@
 package gobley.gradle.cargo.tasks
 
 import gobley.gradle.InternalGobleyGradleApi
+import gobley.gradle.cargo.CargoMessage
 import gobley.gradle.cargo.profiles.CargoProfile
 import gobley.gradle.rust.CrateType
 import gobley.gradle.rust.targets.RustNativeTarget
@@ -23,6 +24,7 @@ import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.OutputFiles
 import org.gradle.api.tasks.TaskAction
+import java.io.File
 
 @Suppress("LeakingThis")
 @CacheableTask
@@ -44,7 +46,8 @@ abstract class CargoBuildTask : CargoPackageTask() {
         profile.zip(target, ::Pair).zip(cargoPackage) { (profile, target), cargoPackage ->
             cargoPackage.libraryCrateTypes.mapNotNull { crateType ->
                 crateType to cargoPackage.outputDirectory(profile, target).file(
-                    target.outputFileName(cargoPackage.libraryCrateName, crateType) ?: return@mapNotNull null
+                    target.outputFileName(cargoPackage.libraryCrateName, crateType)
+                        ?: return@mapNotNull null
                 )
             }.toMap()
         }
@@ -67,6 +70,7 @@ abstract class CargoBuildTask : CargoPackageTask() {
                 }
             }
             arguments("--lib")
+            arguments("--message-format", "json")
             for (extraArgument in extraArguments.get()) {
                 arguments(extraArgument)
             }
@@ -76,34 +80,80 @@ abstract class CargoBuildTask : CargoPackageTask() {
             }
             suppressXcodeIosToolchains()
             if (nativeStaticLibsDefFile.isPresent) {
-                captureStandardError()
+                captureStandardOutput()
             }
         }.get().apply {
             assertNormalExitValue()
         }
 
         if (nativeStaticLibsDefFile.isPresent) {
-            nativeStaticLibsDefFile.get().asFile.run {
+            val nativeStaticLibsDefFile = nativeStaticLibsDefFile.get().asFile.apply {
                 parentFile?.mkdirs()
-
-                val linkerOptName = if (target is RustNativeTarget) {
-                    "linkerOpts.${target.cinteropName}"
-                } else {
-                    "linkerOpts"
-                }
-                val linkerFlag = result.standardError!!.split('\n')
-                    .map { it.trim().substringAfter("note: native-static-libs: ", "") }.firstOrNull(String::isNotEmpty)
-
-                writeText(StringBuilder().apply {
-                    if (linkerFlag != null) {
-                        append("$linkerOptName = $linkerFlag\n")
-                    }
-                    val libraryFile = libraryFileByCrateType.get()[CrateType.SystemStaticLibrary]
-                    if (libraryFile != null) {
-                        append("staticLibraries = ${libraryFile.asFile.name}")
-                    }
-                }.toString())
             }
+            val messages = result.standardOutput!!.split('\n')
+                .mapNotNull { runCatching { CargoMessage(it) }.getOrNull() }
+
+            val librarySearchPaths = mutableListOf<String>()
+            var staticLibraries: String? = null
+            for (message in messages) {
+                when (message) {
+                    is CargoMessage.BuildScriptExecuted -> {
+                        val buildScriptOutput = File(message.outDir).parentFile?.resolve("output")
+                            ?.readLines(Charsets.UTF_8)
+                        if (buildScriptOutput != null) {
+                            for (line in buildScriptOutput) {
+                                val searchPath = line.substringAfter("cargo:", "").trim(':')
+                                    .substringAfter("rustc-link-search=", "")
+                                    .takeIf(String::isNotEmpty)?.split('=')
+                                when (searchPath?.size) {
+                                    1 -> librarySearchPaths.add(searchPath[0])
+                                    2 -> if (searchPath[1] != "crate" && searchPath[1] != "dependency") {
+                                        librarySearchPaths.add(searchPath[1])
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+
+                    is CargoMessage.CompilerMessage -> {
+                        if (staticLibraries == null) {
+                            val note = message.message.rendered?.trim()
+                            staticLibraries =
+                                note?.trim()?.substringAfter("note: native-static-libs: ", "")
+                                    ?.takeIf(String::isNotEmpty)
+                        }
+                    }
+
+                    else -> {}
+                }
+            }
+
+            val linkerOptName = if (target is RustNativeTarget) {
+                "linkerOpts.${target.cinteropName}"
+            } else {
+                "linkerOpts"
+            }
+            val linkerFlag = StringBuilder().apply {
+                if (librarySearchPaths.isNotEmpty()) {
+                    append(librarySearchPaths.joinToString(" ") { "-L$it" })
+                    if (staticLibraries != null) {
+                        append(' ')
+                    }
+                }
+                if (staticLibraries != null) {
+                    append(staticLibraries)
+                }
+            }
+            nativeStaticLibsDefFile.writeText(StringBuilder().apply {
+                if (linkerFlag.isNotEmpty()) {
+                    append("$linkerOptName = $linkerFlag\n")
+                }
+                val libraryFile = libraryFileByCrateType.get()[CrateType.SystemStaticLibrary]
+                if (libraryFile != null) {
+                    append("staticLibraries = ${libraryFile.asFile.name}")
+                }
+            }.toString())
         }
     }
 }

build-logic/gobley-gradle-cargo/src/main/kotlin/CargoMessage.kt
@@ -1,0 +1,134 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package gobley.gradle.cargo
+
+import gobley.gradle.rust.CrateType
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonClassDiscriminator
+
+/**
+ * A JSON format message emitted from `cargo build --message-format json`.
+ * Forked from the [`cargo_metadata`](https://github.com/oli-obk/cargo_metadata/blob/0b4be024c57d7855a2dfbdf2ec2a48f3e16e9f78/src/messages.rs#L225-L245) crate.
+ */
+@OptIn(ExperimentalSerializationApi::class)
+@Serializable
+@JsonClassDiscriminator("reason")
+internal sealed class CargoMessage {
+    @Serializable
+    @SerialName("compiler-artifact")
+    data class CompilerArtifact(
+        @SerialName("package_id") val packageId: String,
+        @SerialName("target") val target: Target,
+        @SerialName("profile") val profile: ArtifactProfile,
+        @SerialName("features") val features: List<String>,
+        @SerialName("filenames") val filenames: List<String>,
+        @SerialName("executable") val executable: String? = null,
+        @SerialName("fresh") val fresh: Boolean,
+    ) : CargoMessage()
+
+    @Serializable
+    @SerialName("compiler-message")
+    data class CompilerMessage(
+        @SerialName("package_id") val packageId: String,
+        @SerialName("target") val target: Target,
+        @SerialName("message") val message: Diagnostic,
+    ) : CargoMessage()
+
+    @Serializable
+    @SerialName("build-script-executed")
+    data class BuildScriptExecuted(
+        @SerialName("package_id") val packageId: String,
+        @SerialName("linked_libs") val linkedLibs: List<String>,
+        @SerialName("linked_paths") val linkedPaths: List<String>,
+        @SerialName("cfgs") val cfgs: List<String>,
+        @SerialName("env") val env: List<List<String>>,
+        @SerialName("out_dir") val outDir: String,
+    ) : CargoMessage()
+
+    @Serializable
+    @SerialName("build-finished")
+    data class BuildFinished(val success: Boolean) : CargoMessage()
+
+    @Serializable
+    data class Target(
+        @SerialName("name") val name: String,
+        @SerialName("kind") val kind: List<CargoTargetKind>,
+        @SerialName("crate_types") val crateTypes: List<CrateType>,
+        @SerialName("src_path") val srcPath: String,
+        @SerialName("edition") val edition: String,
+        @SerialName("doctest") val doctest: Boolean = false,
+        @SerialName("test") val test: Boolean = true,
+    )
+
+    @Serializable
+    data class ArtifactProfile(
+        @SerialName("opt_level") val optLevel: String,
+        @SerialName("debuginfo") val debuginfo: Int? = null,
+        @SerialName("debug_assertions") val debugAssertions: Boolean,
+        @SerialName("overflow_checks") val overflowChecks: Boolean,
+        @SerialName("test") val test: Boolean,
+    )
+
+    @Serializable
+    data class Diagnostic(
+        @SerialName("message") val message: String,
+        @SerialName("code") val code: DiagnosticCode?,
+        @SerialName("level") val level: String,
+        @SerialName("spans") val spans: List<DiagnosticSpan>,
+        @SerialName("children") val children: List<Diagnostic>,
+        @SerialName("rendered") val rendered: String?,
+    )
+
+    @Serializable
+    data class DiagnosticCode(
+        @SerialName("code") val code: String,
+        @SerialName("explanation") val explanation: String?,
+    )
+
+    @Serializable
+    data class DiagnosticSpan(
+        @SerialName("file_name") val fileName: String,
+        @SerialName("byte_start") val byteStart: Int,
+        @SerialName("byte_end") val byteEnd: Int,
+        @SerialName("line_start") val lineStart: Int,
+        @SerialName("line_end") val lineEnd: Int,
+        @SerialName("column_start") val columnStart: Int,
+        @SerialName("column_end") val columnEnd: Int,
+        @SerialName("is_primary") val isPrimary: Boolean,
+        @SerialName("text") val text: List<DiagnosticSpanLine>,
+        @SerialName("label") val label: String?,
+        @SerialName("suggested_replacement") val suggestedReplacement: String?,
+        @SerialName("suggestion_applicability") val suggestionApplicability: String?,
+        @SerialName("expansion") val expansion: DiagnosticSpanExpansion?,
+    )
+
+    @Serializable
+    data class DiagnosticSpanLine(
+        @SerialName("text") val text: String,
+        @SerialName("highlight_start") val highlightStart: Int,
+        @SerialName("highlight_end") val highlightEnd: Int,
+    )
+
+    @Serializable
+    data class DiagnosticSpanExpansion(
+        @SerialName("span") val span: DiagnosticSpan,
+        @SerialName("macro_decl_name") val macroDeclName: String,
+        @SerialName("def_site_span") val defSiteSpan: DiagnosticSpan?,
+    )
+}
+
+internal fun CargoMessage(message: String): CargoMessage {
+    return cargoMessageJson.decodeFromString(message)
+}
+
+private val cargoMessageJson = Json {
+    ignoreUnknownKeys = true
+    classDiscriminator = "reason"
+}

commit 9fc289d5115b4e3164b8f0a56dd217f63c517c16

Make CargoBuildTask replace backslashes in paths with slashes when rendering nativeStaticLibsDefFile (#183)
## Changes

- This PR fixes the bug introduced by #113. Cinterop considers
backslashes to be escape sequences, even on Windows.
- Made `CargoBuildTask` replace backslashes in static library paths with
slashes when rendering nativeStaticLibsDefFile.
- Made `CargoBuildTask` not log messages related to static library
linking.

## Testing

- Fixed `:tests:gradle:cargo-only` failing on Windows since #113.
<img width="1920" height="1080" alt="image"
src="https://github.com/user-attachments/assets/20e5964b-a86b-4028-8054-e39b78f5f4b1"
/>

## Issues Fixed

Fixes: #173

build-logic/gobley-gradle-cargo/src/main/kotlin/tasks/CargoBuildTask.kt
@@ -88,15 +88,21 @@ abstract class CargoBuildTask : CargoPackageTask() {
                     logger.lifecycle(line)
                     continue
                 }
-
                 if (message !is CargoMessage.CompilerMessage) {
                     continue
                 }
-
+                val renderedMessage = message.message.rendered?.takeIf(String::isNotBlank)
+                    ?: continue
+                if (
+                    arrayOf(
+                        "note: Link against",
+                        "note: native-static-libs:",
+                    ).any(renderedMessage::startsWith)
+                ) continue
                 when (message.message.level) {
-                    "error" -> logger.error(message.message.rendered)
-                    "warning" -> logger.warn(message.message.rendered)
-                    else -> logger.lifecycle(message.message.rendered)
+                    "error" -> logger.error(renderedMessage)
+                    "warning" -> logger.warn(renderedMessage)
+                    else -> logger.lifecycle(renderedMessage)
                 }
             }
             assertNormalExitValueUsingLogger(
@@ -121,9 +127,13 @@ abstract class CargoBuildTask : CargoPackageTask() {
                             ?.readLines(Charsets.UTF_8)
                         if (buildScriptOutput != null) {
                             for (line in buildScriptOutput) {
-                                val searchPath = line.substringAfter("cargo:", "").trim(':')
+                                val searchPath = line
+                                    .substringAfter("cargo:", "")
+                                    .trim(':')
                                     .substringAfter("rustc-link-search=", "")
-                                    .takeIf(String::isNotEmpty)?.split('=')
+                                    .takeIf(String::isNotEmpty)
+                                    ?.replace("\\", "/")
+                                    ?.split('=')
                                 when (searchPath?.size) {
                                     1 -> librarySearchPaths.add(searchPath[0])
                                     2 -> if (searchPath[1] != "crate" && searchPath[1] != "dependency") {