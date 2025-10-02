ec8bfc09d22dff83232a562ad2c513300bc14b7d
Chanjung Kim <chanjungkim@paxbun.dev>
2025-10-03 02:21:10 +0900

    Remove RustBufferByReference (#232)
    RustBufferByReference is not currently being used. See
    https://github.com/mozilla/uniffi-rs/pull/2683 for details.

--- crates/gobley-uniffi-bindgen/src/templates/headers/RustBufferTemplate.h
+++ crates/gobley-uniffi-bindgen/src/templates/headers/RustBufferTemplate.h
@@ -6,13 +6,6 @@ typedef struct RustBuffer
     uint8_t *_Nullable data;
 } RustBuffer;
 
-typedef struct RustBufferByReference
-{
-    int64_t capacity;
-    int64_t len;
-    uint8_t *_Nullable data;
-} RustBufferByReference;
-
 typedef struct ForeignBytes
 {
     int32_t len;
