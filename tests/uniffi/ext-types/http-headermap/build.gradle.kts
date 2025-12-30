8c1ad4d6b722d49c0f87a68f438aee8bfc6750ec
Chanjung Kim <chanjungkim@paxbun.dev>
2025-12-30 21:53:01 +0900

    Don't link transitive Rust libraries into native ext-types tests

--- tests/uniffi/ext-types/http-headermap/build.gradle.kts
+++ tests/uniffi/ext-types/http-headermap/build.gradle.kts
@@ -1,3 +1,16 @@
+import gobley.gradle.rust.dsl.hostNativeTarget
+
 plugins {
     id("uniffi-tests-from-library")
+}
+
+project.afterEvaluate {
+    kotlin {
+        hostNativeTarget {
+            compilations.getByName("main") {
+                cinterops.getByName("rust").definitionFile =
+                    layout.projectDirectory.file("rust.def")
+            }
+        }
+    }
 }
