2cdfc0e2e99a827d9f4412046847130e0b1b6d63
Chanjung Kim <chanjungkim@paxbun.dev>
2025-12-31 01:16:11 +0900

    Don't link transitive Rust libraries into native `ext-types` tests (#266)
    ## Changes
    
    Overrided the cinterop def injected by the Cargo plugin with empty def
    files in ext-types tests.
    
    ## Additional context
    
    #265

--- tests/uniffi/ext-types/custom-types/build.gradle.kts
+++ tests/uniffi/ext-types/custom-types/build.gradle.kts
@@ -1,5 +1,18 @@
+import gobley.gradle.rust.dsl.hostNativeTarget
+
 plugins {
     id("uniffi-tests")
+}
+
+project.afterEvaluate {
+    kotlin {
+        hostNativeTarget {
+            compilations.getByName("main") {
+                cinterops.getByName("rust").definitionFile =
+                    layout.projectDirectory.file("rust.def")
+            }
+        }
+    }
 }
 
 uniffi {
