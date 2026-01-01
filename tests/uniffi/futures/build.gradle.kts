5f0ee2416b17237c4fb6d2a4a175c8a84b0503a0
Chanjung Kim <chanjungkim@paxbun.dev>
2026-01-01 15:34:14 +0900

    Enable the JS target in :tests:uniffi:futures

--- tests/uniffi/futures/build.gradle.kts
+++ tests/uniffi/futures/build.gradle.kts
@@ -1,3 +1,16 @@
 plugins {
     id("uniffi-tests-from-library")
 }
+
+kotlin {
+    js {
+        nodejs()
+        browser {
+            testTask {
+                useKarma {
+                    useChromeHeadlessNoSandbox()
+                }
+            }
+        }
+    }
+}
