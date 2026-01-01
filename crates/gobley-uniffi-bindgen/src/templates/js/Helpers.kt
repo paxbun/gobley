c90b53fd723af87835402e305063fc0b6d1ef4e8
Chanjung Kim <chanjungkim@paxbun.dev>
2026-01-01 17:48:45 +0900

    wip2

--- crates/gobley-uniffi-bindgen/src/templates/js/Helpers.kt
+++ crates/gobley-uniffi-bindgen/src/templates/js/Helpers.kt
@@ -1,1 +1,25 @@
 {% include "ffi/Helpers.kt" %}
+
+internal sealed class UniffiRustCallStatusStruct(
+    public abstract val code: Byte,
+    public abstract val errorBuf: RustBufferByValue,
+) {
+    internal class ByValue(
+
+    )
+
+    internal class ByReference(
+        override
+    )
+}
+
+internal typealias UniffiRustCallStatus = UniffiRustCallStatusStruct.ByReference
+internal typealias UniffiRustCallStatusByValue = UniffiRustCallStatusStruct.ByValue
+
+internal object UniffiRustCallStatusHelper {
+    internal fun allocValue() = UniffiRustCallStatusByValue()
+    internal fun <U> withReference(block: (UniffiRustCallStatus) -> U): U {
+        val status = UniffiRustCallStatus()
+        return block(status)
+    }
+}
--- crates/gobley-uniffi-bindgen/src/templates/js/NamespaceLibraryTemplate.kt
+++ crates/gobley-uniffi-bindgen/src/templates/js/NamespaceLibraryTemplate.kt
@@ -1,14 +1,29 @@
 {%- for def in ci.ffi_definitions() %}
+{%- match def %}
+{%- when FfiDefinition::CallbackFunction(callback) %}
+internal typealias {{ callback.name()|ffi_callback_name }} =
+    (
+        {%- for arg in callback.arguments() %}
+        {{ arg.type_().borrow()|ffi_type_name_by_value(ci) }},
+        {%- endfor %}
+        {%- if callback.has_rust_call_status_arg() -%}
+        uniffiCallStatus: UniffiRustCallStatus,
+        {%- endif %}
+    ) ->
+    {%- if let Some(return_type) = callback.return_type() -%}
+    {{ ' ' }}{{ return_type|ffi_type_name_by_value(ci) }}
+    {%- else -%}
+    {{ " Unit" }}
+    {%- endif %}
+{%- when FfiDefinition::Struct(ffi_struct) %}
+// TODO: {{ ffi_struct.name()|ffi_struct_name }}Struct
+{%- when FfiDefinition::Function(_) %}
+{# functions are handled below #}
+{%- endmatch %}
 {%- endfor %}
 
-internal interface UniffiLib {
-    companion object {
-        internal val WASM_INSTANCE: gobley.wasm.{{ ci.namespace() }}
-
-        internal val INSTANCE: UniffiLib by lazy {
-            
-        }
-    }
+internal object UniffiLib {
+    internal val WASM_INSTANCE: gobley.wasm.{{ ci.namespace() }} 
 
     {% for func in ci.iter_ffi_function_definitions() -%}
     fun {{ func.name() }}(
